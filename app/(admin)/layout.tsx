178c035968f7e296d59e177a8dd5c5b7c6b18600
Richard William <rwilliam.flores@gmail.com>
2025-02-08 21:17:49 +0800

    feat: add layout components for admin and instructor with authentication checks

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,20 @@
+import { auth } from "@/auth";
+import Unauthorized from "@/components/ui/unauthorized";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+
+  if (!session) {
+    return redirect("/login");
+  }
+
+  if (session.user.role !== "admin") {
+    return <Unauthorized />;
+  }
+
+  return <>{children}</>;
+};
+
+export default Layout;
