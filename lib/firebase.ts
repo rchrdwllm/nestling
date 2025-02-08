f9e6099874900ce3eed6714e92ade1ffa24de712
Richard William <rwilliam.flores@gmail.com>
2025-02-08 21:41:09 +0800

    feat: configure Firestore with service account credentials

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,4 +1,5 @@
 import { initFirestore } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
@@ -11,4 +12,10 @@ const firebaseConfig = {
 };
 
 export const firebase = initializeApp(firebaseConfig);
-export const db = initFirestore();
+export const db = initFirestore({
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  }),
+});
