0759b8df53aa29d142b554f6215381f986075e4d
Richard William <rwilliam.flores@gmail.com>
2025-03-01 16:53:17 +0800

    feat: implement cache revalidation on page unload and clean up event listeners

--- components/wrappers/cache-refresher-wrapper.tsx
+++ components/wrappers/cache-refresher-wrapper.tsx
@@ -1,29 +1,28 @@
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { ReactNode, useEffect } from "react";
 
 const CacheRefresherWrapper = ({ children }: { children: ReactNode }) => {
   const pathname = usePathname();
-  const router = useRouter();
+
+  const handleBeforeUnload = () => {
+    fetch("/api/revalidate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ pathname }),
+    });
+  };
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const navEntries = performance.getEntriesByType("navigation");
-      if (navEntries.length > 0 && (navEntries[0] as any).type === "reload") {
-        fetch(`/api/revalidate`, {
-          method: "POST",
-          body: JSON.stringify({ pathname }),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            if (data.success) router.refresh();
-            else console.error(data.error);
-          })
-          .catch(console.error);
-      }
-    }
-  }, [pathname]);
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
 
   return <>{children}</>;
 };
