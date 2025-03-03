43ebc73856502b28d1a86032f52f8cf498a360ee
Richard William <rwilliam.flores@gmail.com>
2025-03-03 14:52:11 +0800

    Added PDF rendering

--- app/(instructor)/instructor-courses/[courseId]/modules/content/[contentId]/page.tsx
+++ app/(instructor)/instructor-courses/[courseId]/modules/content/[contentId]/page.tsx
@@ -1,4 +1,5 @@
-import { getModuleContent } from "@/lib/content";
+import PdfViewer from "@/components/instructor-access/content-page/pdf-viewer";
+import { getContentFile, getModuleContent } from "@/lib/content";
 
 const ContentPage = async ({
   params,
@@ -16,7 +17,7 @@ const ContentPage = async ({
     return <div>Loading...</div>;
   }
 
-  //   const file = content.type === "file" ? await getContentFile(contentId) : null;
+  const file = content.type === "file" ? await getContentFile(contentId) : null;
 
   return (
     <main className="p-6">
@@ -28,7 +29,7 @@ const ContentPage = async ({
         className="flex flex-col gap-4 mt-6"
         dangerouslySetInnerHTML={{ __html: content.content }}
       />
-      {/* {file && <PdfViewer pdfUrl={file.success?.secure_url!} />} */}
+      {file && <PdfViewer pdfUrl={file.success?.secure_url!} />}
     </main>
   );
 };
