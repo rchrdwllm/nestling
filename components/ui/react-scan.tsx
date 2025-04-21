466aeb4b4de2edb69cd9a7c89d36506df99f9ad8
Richard William <rwilliam.flores@gmail.com>
2025-04-21 19:18:39 +0800

    Disabled react scan temporarily

--- components/ui/react-scan.tsx
+++ components/ui/react-scan.tsx
@@ -1,14 +1,12 @@
-// path/to/ReactScanComponent
-
 "use client";
-// react-scan must be imported before react
+
 import { scan } from "react-scan";
 import { JSX, useEffect } from "react";
 
 const ReactScan = (): JSX.Element => {
   useEffect(() => {
     scan({
-      enabled: true,
+      enabled: false,
     });
   }, []);
 
