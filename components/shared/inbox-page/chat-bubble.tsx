a0b338b338323e8d25f33a73c6ad4848551a9bd2
Richard William <rwilliam.flores@gmail.com>
2025-04-21 19:54:49 +0800

    Updated chat bubble time display

--- components/shared/inbox-page/chat-bubble.tsx
+++ components/shared/inbox-page/chat-bubble.tsx
@@ -3,6 +3,7 @@
 import { useCurrentUser } from "@/hooks/use-current-user";
 import { cn } from "@/lib/utils";
 import { useEffect, useMemo } from "react";
+import { format } from "date-fns";
 
 type ChatBubbleProps = {
   message: string;
@@ -16,14 +17,27 @@ const ChatBubble = ({ message, senderId, timestamp }: ChatBubbleProps) => {
     return senderId === user.id;
   }, [user, senderId]);
 
+  const formattedTime = useMemo(() => {
+    const date = new Date(timestamp);
+    return format(date, "hh:mm a");
+  }, [timestamp]);
+
   return (
     <article
       className={cn(
-        "flex gap-2 rounded-md p-2 w-max max-w-[55%] text-sm",
+        "flex items-end gap-2 rounded-md p-2 w-max max-w-[55%] text-sm",
         isSender ? "ml-auto bg-primary text-primary-foreground" : "bg-secondary"
       )}
     >
-      {senderId}: {message} at {timestamp}
+      <p>{message}</p>
+      <span
+        className={cn(
+          "text-xs -mb-1",
+          isSender ? "text-white/50" : "text-foreground/50"
+        )}
+      >
+        {formattedTime}
+      </span>
     </article>
   );
 };
