27f522ea59fbfbdcdfcf929a851a1ae48a2396b4
Richard William <rwilliam.flores@gmail.com>
2025-04-27 19:00:53 +0800

    Added loading UI for inbox

---
+++ app/(instructor)/instructor-inbox/loading.tsx
@@ -0,0 +1,78 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+const Loading = () => {
+  return (
+    <div className="overflow-y-hidden h-[calc(100vh-1rem-72.8px)]">
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+      <article className="p-4 border-b flex flex-col gap-2">
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </article>
+    </div>
+  );
+};
+
+export default Loading;
