3b37f979a3fe006b2b16ce26ea3ba4b0b5dd1df1
rwilliam.flores <rwilliam.flores@gmail.com>
2025-04-28 03:22:21 +0000

    Updated loading UI for inbox

--- app/(student)/student-inbox/layout.tsx
+++ app/(student)/student-inbox/layout.tsx
@@ -1,6 +1,7 @@
 import Threads from "@/components/shared/inbox-page/threads";
 import SearchBar from "@/components/shared/search/search-bar";
-import { ReactNode } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { ReactNode, Suspense } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
@@ -9,6 +10,83 @@ const Layout = ({ children }: { children: ReactNode }) => {
         <div className="p-4 border-b border-border">
           <SearchBar isInbox entities={["students", "instructors"]} />
         </div>
+        <Suspense fallback={
+          <aside className="flex flex-col col-span-2 border rounded-xl border-border bg-background">
+          <div className="p-4 border-b border-border">
+            <SearchBar isInbox entities={["students", "instructors"]} />
+          </div>
+          <div className="overflow-y-hidden h-[calc(100vh-1rem-72.8px)]">
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+            <article className="p-4 border-b flex flex-col gap-2">
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-full h-4" />
+            </article>
+          </div>
+        </aside>
+        }></Suspense>
         <Threads />
       </aside>
       <div className="col-span-6 bg-background border border-border rounded-xl">
