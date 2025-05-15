9a2ffb9304457285ca31f4a2fff6e3ec72524da4
Richard William <rwilliam.flores@gmail.com>
2025-05-15 18:17:34 +0800

    Removed unused component

--- app/dashboard/@student/page.tsx
+++ app/dashboard/@student/page.tsx
@@ -21,33 +21,6 @@ const StudentDashboardPage = () => {
         <div className="w-full lg:w-3/4 flex justify-center items-center">
           <Announcements />
         </div>
-        <div className="max-w-[400px] lg:sticky lg:top-4 lg:bg-transparent bg-background rounded-lg shadow-md p-6 lg:p-0 lg:shadow-none lg:mt-0 mt-6">
-          <SidePanel
-            notifications={[
-              {
-                id: "1234-asdfasdf",
-                type: "inbox",
-                title: "From John Doe",
-                message: "asdfalksdghsg",
-                senderId: "use-uiry2i3uhfs",
-                url: "/inbox",
-                createdAt: new Date().toISOString(),
-                isRead: false,
-                isArchived: false,
-                archivedAt: null,
-                receiverIds: ["use-uiasdsdrew"],
-              },
-            ]}
-            pendingTasks={[
-              {
-                name: "Assignment 1.0 - Task Name",
-                courseCode: "CS101",
-                courseName: "Introduction to Computer Science",
-                timeAgo: "8 hours ago",
-              },
-            ]}
-          />
-        </div>
       </div>
     </div>
   );
