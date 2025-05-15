9abba4d11303e748c4c6916187b186685ef7918c
Richard William <rwilliam.flores@gmail.com>
2025-05-15 18:14:15 +0800

    Removed unsused directories

--- app/(instructor)/instructor-calendar/page.tsx
+++
@@ -1,7 +0,0 @@
-import FullCalendar from "@/components/ui/full-calendar";
-
-const InstructorCalendarPage = async () => {
-  return <FullCalendar events={[]} />;
-};
-
-export default InstructorCalendarPage;
--- app/(instructor)/instructor-inbox/page.tsx
+++
@@ -1,9 +0,0 @@
-const InstructorInbox = () => {
-  return (
-    <main className="h-full flex justify-center items-center">
-      <h1 className="text-muted-foreground">Start chatting with someone</h1>
-    </main>
-  );
-};
-
-export default InstructorInbox;
