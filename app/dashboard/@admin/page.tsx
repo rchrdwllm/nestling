3af3e7637ddeb6c9a368866aabeb7d4620179e84
Richard William <rwilliam.flores@gmail.com>
2025-05-21 17:13:10 +0800

    Updated page layout

--- app/dashboard/@admin/page.tsx
+++ app/dashboard/@admin/page.tsx
@@ -11,7 +11,10 @@ const AdminDashboardPage = async () => {
 
   return (
     <div className="p-6 flex flex-col gap-4">
-      <h1 className="text-3xl font-semibold">Admin Dashboard</h1>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-semibold">Admin Dashboard</h1>
+        <hr />
+      </div>
       <div className="grid grid-cols-3 gap-4">
         <TotalStudentsOverview />
         <TotalInstructorsOverview />
