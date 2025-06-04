7f4b0317455838c53099406f047bf99d17a788e4
Richard William <rwilliam.flores@gmail.com>
2025-06-05 07:37:18 +0800

    Added loading skeleton for projects page

---
+++ app/projects/@admin/loading.tsx
@@ -0,0 +1,31 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+const Loading = async () => {
+  return (
+    <main className="h-full p-6 flex flex-col gap-4">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-semibold">Projects Dashboard</h1>
+        <hr />
+      </div>
+      <section className="grid grid-cols-2 gap-4 pb-6">
+        <article className="col-span-2">
+          <Skeleton className="w-full h-52" />
+        </article>
+        <article className="col-span-2">
+          <Skeleton className="w-full h-52" />
+        </article>
+        <article className="min-h-[378px]">
+          <Skeleton className="size-full" />
+        </article>
+        <article className="min-h-[378px]">
+          <Skeleton className="size-full" />
+        </article>
+        <article className="min-h-[378px] col-span-2">
+          <Skeleton className="size-full" />
+        </article>
+      </section>
+    </main>
+  );
+};
+
+export default Loading;
