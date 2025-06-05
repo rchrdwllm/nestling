c4444cb42017acc0cbf9fa180693383c939e43a7
Richard William <rwilliam.flores@gmail.com>
2025-06-05 20:53:29 +0800

    Added responsiveness for side panel

--- components/shared/layout/side-panel.tsx
+++ components/shared/layout/side-panel.tsx
@@ -139,7 +139,7 @@ const SidePanel = ({
               x: rightPanelToggled ? 0 : "100%",
             }}
             exit={{ opacity: 0, x: "100%" }}
-            className="w-full border border-border rounded-xl lg:w-[400px] lg:sticky relative bg-background lg:p-0 shadow-sm"
+            className="absolute right-2 lg:right-auto h-[calc(100%-1rem)] border border-border rounded-xl w-[314px] lg:h-auto lg:w-[400px] lg:relative bg-background lg:p-0 shadow-sm"
           >
             <ScrollArea className="h-full">
               <div className="flex flex-col space-y-6 p-6">
