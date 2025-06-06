4ae64bbe4b3fd5fd0d7404a675fe00607f3529c3
John Andrae Tuaño <draetuano01@gmail.com>
2025-06-07 01:38:16 +0800

    Added loading screen for instructor dashboard

---
+++ app/dashboard/@instructor/loading.tsx
@@ -0,0 +1,20 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function InstructorDashboardLoading() {
+  return (
+    <div className="p-6 flex flex-col gap-8">
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center gap-4">
+          <Skeleton className="h-10 w-40" />
+          <Skeleton className="h-10 flex-1" />
+        </div>
+        <Skeleton className="h-px w-full" />
+      </div>
+      <div className="flex flex-col gap-6">
+        {[...Array(3)].map((_, i) => (
+          <Skeleton key={i} className="h-32 w-full rounded-xl" />
+        ))}
+      </div>
+    </div>
+  );
+}
