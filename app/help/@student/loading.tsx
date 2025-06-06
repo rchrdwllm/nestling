b4254e2c02be31f68e65f20d175f2c4808c3f1e2
Richard William <rwilliam.flores@gmail.com>
2025-06-06 15:29:50 +0800

    Moved loading tickets to subfolders

---
+++ app/help/@student/loading.tsx
@@ -0,0 +1,26 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <Skeleton className="w-full h-full flex flex-col p-6 gap-10">
+      {/* Header skeleton */}
+      <Skeleton className="h-10 w-1/3 mb-2" />
+      <Skeleton className="h-1 w-full mb-6" />
+      {/* FAQ title skeleton */}
+      <Skeleton className="h-8 w-2/5 mb-6" />
+      {/* FAQ items skeleton */}
+      <Skeleton className="flex flex-col gap-4">
+        {[...Array(5)].map((_, i) => (
+          <Skeleton key={i} className="h-8 w-full rounded" />
+        ))}
+      </Skeleton>
+      {/* Button skeleton */}
+      <Skeleton className="flex justify-center mt-8">
+        <Skeleton className="h-12 w-72 rounded-full" />
+      </Skeleton>
+    </Skeleton>
+  );
+};
+
+export default Loading;
