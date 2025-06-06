0f9e5bc87af2a5de3440d53e27c23a9ab7033139
John Andrae Tuaño <draetuano01@gmail.com>
2025-06-07 03:55:56 +0800

    Added animation wrapper in student dashboard

--- app/dashboard/@student/page.tsx
+++ app/dashboard/@student/page.tsx
@@ -1,22 +1,25 @@
+import FadeInWrapper from "@/components/wrappers/fadein-wrapper";
 import SearchBar from "@/components/shared/search/search-bar";
 import MyCourses from "@/components/student-access/dashboard-page/my-courses";
 import RecentAnnouncements from "@/components/student-access/dashboard-page/recent-announcements";
 
 const StudentDashboardPage = () => {
   return (
-    <div className="p-6 flex flex-col gap-8">
-      <div className="flex flex-col gap-4">
-        <div className="flex items-center gap-4">
-          <h1 className="text-3xl font-semibold">Dashboard</h1>
-          <div className="flex-1">
-            <SearchBar entities={["students", "courses"]} />
+    <FadeInWrapper>
+      <div className="p-6 flex flex-col gap-8">
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center gap-4">
+            <h1 className="text-3xl font-semibold">Dashboard</h1>
+            <div className="flex-1">
+              <SearchBar entities={["students", "courses"]} />
+            </div>
           </div>
+          <hr />
         </div>
-        <hr />
+        <MyCourses />
+        <RecentAnnouncements />
       </div>
-      <MyCourses />
-      <RecentAnnouncements />
-    </div>
+    </FadeInWrapper>
   );
 };
 
