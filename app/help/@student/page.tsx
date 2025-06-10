6656fa1d21a388643a40f6e14b37ea809964f62c
Richard William <rwilliam.flores@gmail.com>
2025-06-10 14:34:32 +0800

    Removed promise in help page

--- app/help/@student/page.tsx
+++ app/help/@student/page.tsx
@@ -2,8 +2,6 @@ import Help from "@/components/shared/help-page/faq";
 import UserManual from "@/components/shared/help-page/user-manual";
 
 const HelpPage = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 700));
-
   return (
     <div className="flex flex-col gap-8 p-6">
       <div className="flex flex-col gap-4">
