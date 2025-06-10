cd498b13d1d5442b9ade0378911a99a444e67425
Richard William <rwilliam.flores@gmail.com>
2025-06-10 12:48:58 +0800

    Updated loading component to take height

--- app/profile/loading.tsx
+++ app/profile/loading.tsx
@@ -2,10 +2,14 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
   return (
-    <div className="h-full justify-center items-center">
+    <div className="h-full flex justify-center items-center">
       <div>
         <div className="flex flex-col gap-8 items-center">
           <Skeleton className="size-32 rounded-full" />
+          <div className="flex flex-col gap-4">
+            <Skeleton className="w-56 h-9" />
+            <Skeleton className="w-16 h-4" />
+          </div>
           <Skeleton className="w-[149px] h-[93px]" />
           <div className="flex flex-col gap-4 items-start">
             <Skeleton className="h-6 w-[315px]" />
