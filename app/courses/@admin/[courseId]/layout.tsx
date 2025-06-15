c04e7f3e5b4dde2a777b49f9648d9280422ad1d3
Richard William <rwilliam.flores@gmail.com>
2025-06-15 23:03:09 +0800

    Updated courses sidebar

--- app/courses/@admin/[courseId]/layout.tsx
+++ app/courses/@admin/[courseId]/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import CourseSectionLink from "@/components/shared/courses-page/course-section-link";
 import { getCourse } from "@/lib/course";
 import ErrorToast from "@/components/ui/error-toast";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const Layout = async ({
   children,
@@ -18,65 +19,69 @@ const Layout = async ({
   }
 
   return (
-    <section className="grid grid-cols-10 gap-x-2">
-      <aside className="p-6 flex flex-col gap-2 col-span-2">
-        <h1 className="font-medium text-sm">
-          {course.courseCode} - {course.name}
-        </h1>
-        <CourseSectionLink href={`/courses/${courseId}`}>
-          Modules
-        </CourseSectionLink>
-        <CourseSectionLink
-          segments={["assignments"]}
-          href={`/courses/${courseId}/assignments`}
-        >
-          Assignments
-        </CourseSectionLink>
-        <CourseSectionLink
-          segments={["discussions"]}
-          href={`/courses/${courseId}/discussions`}
-        >
-          Discussions
-        </CourseSectionLink>
-        <CourseSectionLink
-          segments={["announcements"]}
-          href={`/courses/${courseId}/announcements`}
-        >
-          Announcements
-        </CourseSectionLink>
-        <CourseSectionLink
-          segments={["people"]}
-          href={`/courses/${courseId}/people`}
-        >
-          People
-        </CourseSectionLink>
-        <CourseSectionLink
-          segments={["archive"]}
-          href={`/courses/${courseId}/archive`}
-        >
-          Archived Modules
-        </CourseSectionLink>
-        <CourseSectionLink
-          segments={["discussions", "archive"]}
-          href={`/courses/${courseId}/discussions/archive`}
-        >
-          Archived Discussions
-        </CourseSectionLink>
-        <CourseSectionLink
-          segments={["announcements", "archive"]}
-          href={`/courses/${courseId}/announcements/archive`}
-        >
-          Archived Announcements
-        </CourseSectionLink>
-        <CourseSectionLink
-          segments={["grade"]}
-          href={`/courses/${courseId}/grade`}
-        >
-          Grade
-        </CourseSectionLink>
-      </aside>
-      <div className="col-span-8">{children}</div>
-    </section>
+    <main className="gap-x-2 grid grid-cols-8 bg-secondary">
+      <ScrollArea className="col-span-2 h-[calc(100vh-1rem)] sidebar-scroll-area">
+        <aside className="flex flex-col gap-2 col-span-2 bg-background shadow-sm p-6 border border-border rounded-xl h-full">
+          <h1 className="font-medium text-sm">
+            {course.courseCode} - {course.name}
+          </h1>
+          <CourseSectionLink href={`/courses/${courseId}`}>
+            Modules
+          </CourseSectionLink>
+          <CourseSectionLink
+            segments={["assignments"]}
+            href={`/courses/${courseId}/assignments`}
+          >
+            Assignments
+          </CourseSectionLink>
+          <CourseSectionLink
+            segments={["discussions"]}
+            href={`/courses/${courseId}/discussions`}
+          >
+            Discussions
+          </CourseSectionLink>
+          <CourseSectionLink
+            segments={["announcements"]}
+            href={`/courses/${courseId}/announcements`}
+          >
+            Announcements
+          </CourseSectionLink>
+          <CourseSectionLink
+            segments={["people"]}
+            href={`/courses/${courseId}/people`}
+          >
+            People
+          </CourseSectionLink>
+          <CourseSectionLink
+            segments={["archive"]}
+            href={`/courses/${courseId}/archive`}
+          >
+            Archived Modules
+          </CourseSectionLink>
+          <CourseSectionLink
+            segments={["discussions", "archive"]}
+            href={`/courses/${courseId}/discussions/archive`}
+          >
+            Archived Discussions
+          </CourseSectionLink>
+          <CourseSectionLink
+            segments={["announcements", "archive"]}
+            href={`/courses/${courseId}/announcements/archive`}
+          >
+            Archived Announcements
+          </CourseSectionLink>
+          <CourseSectionLink
+            segments={["grade"]}
+            href={`/courses/${courseId}/grade`}
+          >
+            Grade
+          </CourseSectionLink>
+        </aside>
+      </ScrollArea>
+      <ScrollArea className="col-span-6 bg-background shadow-sm border border-border rounded-xl h-[calc(100vh-1rem)] overflow-y-auto">
+        {children}
+      </ScrollArea>
+    </main>
   );
 };
 
