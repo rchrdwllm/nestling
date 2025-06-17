71ece9d6c6928fe6516e9854ac8432878342f724
John Andrae Tuaño <draetuano01@gmail.com>
2025-06-17 20:17:59 +0800

    Updated the notification card styles to improve layout and responsiveness

--- components/shared/layout/notification-card.tsx
+++ components/shared/layout/notification-card.tsx
@@ -4,6 +4,7 @@ import { useAction } from "next-safe-action/hooks";
 import Link from "next/link";
 import { toast } from "sonner";
 
+
 const NotificationCard = ({ title, message, url, id }: Notification) => {
   const { execute } = useAction(readNotif, {
     onSuccess: () => {
@@ -21,14 +22,14 @@ const NotificationCard = ({ title, message, url, id }: Notification) => {
   };
 
   return (
-    <Link
-      onClick={handleNotifClick}
-      href={url}
-      className="after:left-2 after:absolute relative after:inset-y-2 flex flex-col gap-1 bg-muted-secondary after:bg-primary p-2 pl-6 rounded-md after:rounded-full after:w-1 text-sm cursor-pointer"
-    >
-      <h1 className="font-medium">{title}</h1>
-      <p className="text-muted-foreground text-xs">{message}</p>
-    </Link>
+      <Link
+        onClick={handleNotifClick}
+        href={url}
+        className="notification-card after:left-2 after:absolute relative after:inset-y-2 flex flex-col gap-1 bg-muted-secondary after:bg-primary p-2 pl-6 rounded-md after:rounded-full after:w-1 text-sm cursor-pointer"
+      >
+        <h1 className="font-medium">{title}</h1>
+        <p className="text-muted-foreground text-xs">{message}</p>
+      </Link>
   );
 };
 
