a20944578efaea2df2b274a22e971f78ce350f66
Richard William <rwilliam.flores@gmail.com>
2025-06-17 23:23:17 +0800

    Fixed ticket loading

--- app/help/@instructor/tickets/(tickets)/loading.tsx
+++ app/help/@instructor/tickets/(tickets)/loading.tsx
@@ -2,24 +2,23 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
   return (
-    <div className="p-6 flex flex-col gap-8">
+    <div className="flex flex-col gap-8 p-6">
       <div className="flex flex-col gap-4">
         <div className="flex justify-between items-center">
-          <Skeleton className="h-9 w-60" />
+          <Skeleton className="w-60 h-9" />
+          <Skeleton className="w-36 h-9" />
         </div>
         <hr />
       </div>
       <section className="flex flex-col gap-4">
-        <Skeleton className="w-full h-10" />
         <div className="flex justify-between items-center">
-          <Skeleton className="w-full h-10 max-w-md" />
-          <div className="flex items-center gap-2">
-            <Skeleton className="h-10 w-[86px]" />
-            <Skeleton className="h-10 w-[58px]" />
-            <Skeleton className="h-10 w-[85px]" />
+          <Skeleton className="w-full max-w-sm h-10" />
+          <div className="flex items-center gap-4">
+            <Skeleton className="w-28 h-10" />
+            <Skeleton className="w-28 h-10" />
           </div>
         </div>
-        <Skeleton className="w-full h-[400px]" />
+        <Skeleton className="w-full h-96" />
       </section>
     </div>
   );
--- app/help/@student/tickets/(tickets)/loading.tsx
+++ app/help/@student/tickets/(tickets)/loading.tsx
@@ -1,79 +1,24 @@
-import ErrorToast from "@/components/ui/error-toast";
-import { getTicketById } from "@/lib/ticket";
-import { getTicketReplies } from "@/lib/ticket-reply";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Card } from "@/components/ui/card";
 
 const Loading = () => {
   return (
-    <div className="p-6 flex flex-col gap-8">
+    <div className="flex flex-col gap-8 p-6">
       <div className="flex flex-col gap-4">
-        <div className="flex justify-between items-center gap-4">
-          <Skeleton className="h-9 w-60" />
+        <div className="flex justify-between items-center">
+          <Skeleton className="w-60 h-9" />
+          <Skeleton className="w-36 h-9" />
         </div>
         <hr />
       </div>
       <section className="flex flex-col gap-4">
-        <Card className="p-6 flex flex-col gap-4">
+        <div className="flex justify-between items-center">
+          <Skeleton className="w-full max-w-sm h-10" />
           <div className="flex items-center gap-4">
-            <Skeleton className="size-10 rounded-full" />
-            <div>
-              <Skeleton className="h-7 w-64" />
-              <Skeleton className="w-80 h-4" />
-            </div>
+            <Skeleton className="w-28 h-10" />
+            <Skeleton className="w-28 h-10" />
           </div>
-          <div className="ml-14 flex flex-col gap-4">
-            <div className="flex gap-2 items-center">
-              <Skeleton className="h-[21px] w-[55px] rounded-full" />
-              <Skeleton className="h-[21px] w-[55px] rounded-full" />
-              <Skeleton className="h-[21px] w-[55px] rounded-full" />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Skeleton />
-              <Skeleton />
-            </div>
-          </div>
-        </Card>
-        <Card className="p-6 flex flex-col gap-4">
-          <div className="flex items-center gap-4">
-            <Skeleton className="size-10 rounded-full" />
-            <div>
-              <Skeleton className="h-7 w-64" />
-              <Skeleton className="w-80 h-4" />
-            </div>
-          </div>
-          <div className="ml-14 flex flex-col gap-4">
-            <div className="flex gap-2 items-center">
-              <Skeleton className="h-[21px] w-[55px] rounded-full" />
-              <Skeleton className="h-[21px] w-[55px] rounded-full" />
-              <Skeleton className="h-[21px] w-[55px] rounded-full" />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Skeleton />
-              <Skeleton />
-            </div>
-          </div>
-        </Card>
-        <Card className="p-6 flex flex-col gap-4">
-          <div className="flex items-center gap-4">
-            <Skeleton className="size-10 rounded-full" />
-            <div>
-              <Skeleton className="h-7 w-64" />
-              <Skeleton className="w-80 h-4" />
-            </div>
-          </div>
-          <div className="ml-14 flex flex-col gap-4">
-            <div className="flex gap-2 items-center">
-              <Skeleton className="h-[21px] w-[55px] rounded-full" />
-              <Skeleton className="h-[21px] w-[55px] rounded-full" />
-              <Skeleton className="h-[21px] w-[55px] rounded-full" />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Skeleton />
-              <Skeleton />
-            </div>
-          </div>
-        </Card>
+        </div>
+        <Skeleton className="w-full h-96" />
       </section>
     </div>
   );
