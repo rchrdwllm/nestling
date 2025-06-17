870cdc9fefa910ffb6ef8333d030e8b67526ff66
John Andrae Tuaño <draetuano01@gmail.com>
2025-06-17 23:47:34 +0800

    Switched places month/year with current date

--- components/ui/full-calendar.tsx
+++ components/ui/full-calendar.tsx
@@ -661,31 +661,35 @@ export default function FullCalendar({ events }: FullCalendarProps) {
   return (
     <Calendar events={events}>
       <div className="h-dvh py-6 flex flex-col">
-        <div className="flex px-6 items-center gap-2 mb-6">
-          <CalendarViewTrigger
-            view="month"
-            className="aria-[current=true]:bg-accent"
-          >
-            Month
-          </CalendarViewTrigger>
-          <CalendarViewTrigger
-            view="year"
-            className="aria-[current=true]:bg-accent"
-          >
-            Year
-          </CalendarViewTrigger>
-          <span className="flex-1" />
-          <CalendarCurrentDate />
-          <CalendarPrevTrigger>
-            <ChevronLeft size={20} />
-            <span className="sr-only">Previous</span>
-          </CalendarPrevTrigger>
-          <CalendarTodayTrigger>Today</CalendarTodayTrigger>
-          <CalendarNextTrigger>
-            <ChevronRight size={20} />
-            <span className="sr-only">Next</span>
-          </CalendarNextTrigger>
+        <div className="flex items-center gap-2 mb-6 px-6 w-full">
+          <div className="flex items-center gap-2 ml-4">
+            <CalendarCurrentDate />
+            <CalendarPrevTrigger>
+              <ChevronLeft size={20} />
+              <span className="sr-only">Previous</span>
+            </CalendarPrevTrigger>
+            <CalendarTodayTrigger>Today</CalendarTodayTrigger>
+            <CalendarNextTrigger>
+              <ChevronRight size={20} />
+              <span className="sr-only">Next</span>
+            </CalendarNextTrigger>
+          </div>
+          <div className="ml-4 flex items-center gap-2">
+            <CalendarViewTrigger
+              view="month"
+              className="aria-[current=true]:bg-accent"
+            >
+              Month
+            </CalendarViewTrigger>
+            <CalendarViewTrigger
+              view="year"
+              className="aria-[current=true]:bg-accent"
+            >
+              Year
+            </CalendarViewTrigger>
+          </div>
         </div>
+
         <div className="flex-1 overflow-auto px-6 relative">
           <CalendarMonthView />
           <CalendarYearView />
