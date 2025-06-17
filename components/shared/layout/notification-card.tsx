17aa78b510db52d13ad06ee082f30bd5bf6ec54d
John Andrae Tuaño <draetuano01@gmail.com>
2025-06-17 20:34:53 +0800

    Updated the notification and pending task card styles to improve layout and responsiveness

--- components/shared/layout/notification-card.tsx
+++ components/shared/layout/notification-card.tsx
@@ -25,7 +25,7 @@ const NotificationCard = ({ title, message, url, id }: Notification) => {
       <Link
         onClick={handleNotifClick}
         href={url}
-        className="notification-card after:left-2 after:absolute relative after:inset-y-2 flex flex-col gap-1 bg-muted-secondary after:bg-primary p-2 pl-6 rounded-md after:rounded-full after:w-1 text-sm cursor-pointer"
+        className="side-panel-card after:left-2 after:absolute relative after:inset-y-2 flex flex-col gap-1 bg-muted-secondary after:bg-primary p-2 pl-6 rounded-md after:rounded-full after:w-1 text-sm cursor-pointer"
       >
         <h1 className="font-medium">{title}</h1>
         <p className="text-muted-foreground text-xs">{message}</p>
