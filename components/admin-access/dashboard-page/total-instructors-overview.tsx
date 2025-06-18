aae7d2f90bf1e30a8fb408226e02f4942e9d09ba
Richard William <rwilliam.flores@gmail.com>
2025-06-18 08:48:48 +0800

    Updated count for displaying

--- components/admin-access/dashboard-page/total-instructors-overview.tsx
+++ components/admin-access/dashboard-page/total-instructors-overview.tsx
@@ -1,10 +1,10 @@
-import { getUnarchivedInstructors } from "@/lib/user";
+import { getAllInstructors } from "@/lib/user";
 import DateDisplay from "../../ui/date-display";
 import ErrorToast from "@/components/ui/error-toast";
 
 const TotalInstructorsOverview = async () => {
   const { success: allInstructors, error: allInstructorsError } =
-    await getUnarchivedInstructors();
+    await getAllInstructors();
 
   if (allInstructorsError || !allInstructors) {
     return (
@@ -15,8 +15,8 @@ const TotalInstructorsOverview = async () => {
   }
 
   return (
-    <article className="border border-border rounded-xl p-4 flex flex-col gap-2 shadow-sm">
-      <p className="text-sm font-medium">Total instructors</p>
+    <article className="flex flex-col gap-2 shadow-sm p-4 border border-border rounded-xl">
+      <p className="font-medium text-sm">Total instructors</p>
       <h1 className="font-mono font-semibold text-4xl">
         {allInstructors.length}
       </h1>
--- components/admin-access/dashboard-page/total-students-overview.tsx
+++ components/admin-access/dashboard-page/total-students-overview.tsx
@@ -1,10 +1,11 @@
-import { getUnarchivedStudents } from "@/lib/user";
+import { getAllStudents } from "@/lib/user";
 import DateDisplay from "../../ui/date-display";
 import ErrorToast from "@/components/ui/error-toast";
 
 const TotalStudentsOverview = async () => {
   const { success: allStudents, error: allStudentsError } =
-    await getUnarchivedStudents();
+    await getAllStudents();
+
   if (allStudentsError || !allStudents) {
     return (
       <ErrorToast
@@ -14,8 +15,8 @@ const TotalStudentsOverview = async () => {
   }
 
   return (
-    <article className="border border-border shadow-sm rounded-xl p-4 flex flex-col gap-2">
-      <p className="text-sm font-medium">Total students</p>
+    <article className="flex flex-col gap-2 shadow-sm p-4 border border-border rounded-xl">
+      <p className="font-medium text-sm">Total students</p>
       <h1 className="font-mono font-semibold text-4xl">{allStudents.length}</h1>
       <p className="text-muted-foreground text-sm">
         Last updated:{" "}
