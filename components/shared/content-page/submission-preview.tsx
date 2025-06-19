b7d2392042588bdf9002541c773cb539e9f68800
Richard William <rwilliam.flores@gmail.com>
2025-06-19 21:09:01 +0800

    Fixed rendering of text submissions when viewing

--- components/shared/content-page/submission-preview.tsx
+++ components/shared/content-page/submission-preview.tsx
@@ -3,6 +3,7 @@ import SubmissionAttempt from "@/components/shared/courses-page/submissions/subm
 import PdfViewer from "@/components/shared/content-page/pdf-viewer";
 import { getModuleContent } from "@/lib/content";
 import { getFile, verifyFileIntegrity } from "@/lib/file";
+import ErrorToast from "@/components/ui/error-toast";
 
 type SubmissionPreviewProps = {
   studentId: string;
@@ -23,30 +24,63 @@ const SubmissionPreview = async ({
     contentId
   );
 
-  if (studentSubmissionsError || contentError) {
-    return <div>{studentSubmissionsError}</div>;
-  }
-
-  if (!studentSubmissions || !content) {
-    return <div>Loading...</div>;
+  if (
+    studentSubmissionsError ||
+    contentError ||
+    !studentSubmissions ||
+    !content
+  ) {
+    return (
+      <ErrorToast
+        error={
+          "Error fetching submissions or content" +
+          (studentSubmissionsError || contentError)
+        }
+      />
+    );
   }
 
   const selectedAttempt = attempt
     ? studentSubmissions[parseInt(attempt) - 1]
     : studentSubmissions[0];
-  const { success: file, error: fileError } = await getFile(
-    selectedAttempt.fileId
-  );
 
-  if (fileError) {
-    return <div>{fileError}</div>;
+  if (submissionType === "file") {
+    const { success: file, error: fileError } = await getFile(
+      selectedAttempt.fileId
+    );
+
+    if (fileError || !file) {
+      return <ErrorToast error={fileError} />;
+    }
+
+    const isVerified = await verifyFileIntegrity(file.secure_url, file.hash);
+
+    return (
+      <div className="flex-1">
+        <div className="flex flex-col gap-2">
+          {content.maxAttempts! > 0 && studentSubmissions.length > 1
+            ? studentSubmissions.map((submission, index) => (
+                <SubmissionAttempt
+                  key={submission.id}
+                  index={index}
+                  indexLabel={studentSubmissions.length - index - 1}
+                  isLatest={index === 0}
+                />
+              ))
+            : null}
+        </div>
+        {isVerified ? (
+          <div className="mt-6 p-6 border border-border rounded-xl">
+            <PdfViewer pdfUrl={selectedAttempt.secureUrl} />
+          </div>
+        ) : (
+          <div className="bg-red-50 dark:bg-red-950 mt-8 p-6 border border-border rounded-xl text-red-800 dark:text-red-400">
+            <p>This file may have been tampered with</p>
+          </div>
+        )}
+      </div>
+    );
   }
-
-  if (!file) {
-    return <div>Loading...</div>;
-  }
-
-  const isVerified = await verifyFileIntegrity(file.secure_url, file.hash);
 
   return (
     <div className="flex-1">
@@ -62,22 +96,12 @@ const SubmissionPreview = async ({
             ))
           : null}
       </div>
-      {isVerified ? (
-        <div className="p-6 border border-border rounded-xl mt-6">
-          {submissionType === "text" ? (
-            <div
-              className="flex flex-col gap-4"
-              dangerouslySetInnerHTML={{ __html: selectedAttempt.content }}
-            />
-          ) : (
-            <PdfViewer pdfUrl={selectedAttempt.secureUrl} />
-          )}
-        </div>
-      ) : (
-        <div className="mt-8 p-6 border border-border rounded-xl bg-red-50 dark:bg-red-950 text-red-800 dark:text-red-400">
-          <p>This file may have been tampered with</p>
-        </div>
-      )}
+      <div className="mt-6 p-6 border border-border rounded-xl">
+        <div
+          className="flex flex-col gap-4"
+          dangerouslySetInnerHTML={{ __html: selectedAttempt.content }}
+        />
+      </div>
     </div>
   );
 };
