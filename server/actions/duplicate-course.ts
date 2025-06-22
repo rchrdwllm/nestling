5f88dd434268e11eca5ee8a836bb1ad973c32d0d
Richard William <rwilliam.flores@gmail.com>
2025-06-22 20:25:43 +0800

    Fixed duplication of course

--- server/actions/duplicate-course.ts
+++ server/actions/duplicate-course.ts
@@ -188,6 +188,7 @@ export const duplicateCourse = actionClient
             ...content,
             id: newContentId,
             moduleId: newModuleId,
+            courseId: newId,
             createdAt: new Date().toISOString(),
             updatedAt: new Date().toISOString(),
           });
