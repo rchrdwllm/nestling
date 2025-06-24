d070afb82fb25338d8af9f9e1e5f09a5e135ba97
Richard William <rwilliam.flores@gmail.com>
2025-06-24 13:59:50 +0800

    Added next and previous buttons to course module content pages

--- app/courses/@admin/[courseId]/modules/content/[contentId]/page.tsx
+++ app/courses/@admin/[courseId]/modules/content/[contentId]/page.tsx
@@ -1,12 +1,17 @@
 import AssignmentDetails from "@/components/shared/courses-page/assignment-content/assignment-details";
 import PdfViewer from "@/components/shared/content-page/pdf-viewer";
 import { Button } from "@/components/ui/button";
-import { getContentFile, getModuleContent } from "@/lib/content";
+import {
+  getContentFile,
+  getModuleContent,
+  getModuleContents,
+} from "@/lib/content";
 import Link from "next/link";
 import ErrorToast from "@/components/ui/error-toast";
 import Searcher from "@/components/shared/search/general-search/searcher";
 import DeleteContentBtn from "@/components/shared/content-page/delete-content-btn";
 import { addAttachmentFlag } from "@/lib/utils";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const ContentPage = async ({
   params,
@@ -23,41 +28,85 @@ const ContentPage = async ({
     return <ErrorToast error={"Error fetching content: " + (error || "")} />;
   }
 
-  console.log("Content fetched:", content);
-
   const file = content.type === "file" ? await getContentFile(contentId) : null;
 
+  const { success: allContents, error: allContentsError } =
+    await getModuleContents(content.moduleId);
+
+  if (allContentsError || !allContents) {
+    return (
+      <ErrorToast
+        error={"Error fetching module contents: " + allContentsError}
+      />
+    );
+  }
+
+  const currentIdx = allContents?.findIndex((c) => c.id === contentId) ?? -1;
+  const prevContent = currentIdx > 0 ? allContents[currentIdx - 1] : null;
+  const nextContent =
+    allContents && currentIdx < allContents.length - 1
+      ? allContents[currentIdx + 1]
+      : null;
+
   return (
-    <main className="flex gap-16 p-6">
-      <Searcher query={query} page={page} tab={tab} />
-      <div className="flex-1">
-        <div className="flex flex-col gap-3">
-          <div className="flex justify-between items-center gap-4">
-            <h1 className="flex-1 font-semibold text-3xl">{content.title}</h1>
-            <DeleteContentBtn contentId={contentId} />
-            <Link
-              href={`/courses/${courseId}/create?moduleId=${content.moduleId}&contentId=${contentId}`}
-            >
-              <Button variant="outline">Edit content</Button>
-            </Link>
+    <main className="flex flex-col gap-8 p-6">
+      <section className="flex gap-16">
+        <Searcher query={query} page={page} tab={tab} />
+        <div className="flex-1">
+          <div className="flex flex-col gap-3">
+            <div className="flex justify-between items-center gap-4">
+              <h1 className="flex-1 font-semibold text-3xl">{content.title}</h1>
+              <DeleteContentBtn contentId={contentId} />
+              <Link
+                href={`/courses/${courseId}/create?moduleId=${content.moduleId}&contentId=${contentId}`}
+              >
+                <Button variant="outline">Edit content</Button>
+              </Link>
+            </div>
+            <hr />
           </div>
-          <hr />
+          {content.type === "assignment" && <AssignmentDetails {...content} />}
+          <div
+            className="flex flex-col gap-4 mt-6"
+            dangerouslySetInnerHTML={{ __html: content.content }}
+          />
+          {file && (
+            <PdfViewer pdfUrl={addAttachmentFlag(file.success?.secure_url!)} />
+          )}
         </div>
-        {content.type === "assignment" && <AssignmentDetails {...content} />}
-        <div
-          className="flex flex-col gap-4 mt-6"
-          dangerouslySetInnerHTML={{ __html: content.content }}
-        />
-        {file && (
-          <PdfViewer pdfUrl={addAttachmentFlag(file.success?.secure_url!)} />
+        {content.type === "assignment" && (
+          <Link
+            href={`/courses/${content.courseId}/modules/content/${contentId}/submissions`}
+          >
+            <Button variant="link">View submissions</Button>
+          </Link>
         )}
-      </div>
-      {content.type === "assignment" && (
-        <Link
-          href={`/courses/${content.courseId}/modules/content/${contentId}/submissions`}
-        >
-          <Button variant="link">View submissions</Button>
-        </Link>
+      </section>
+      {(prevContent || nextContent) && (
+        <>
+          <hr />
+          <section className="flex justify-between items-center">
+            {prevContent && (
+              <Link
+                href={`/courses/${content.courseId}/modules/content/${prevContent.id}`}
+              >
+                <Button variant="outline">
+                  <ChevronLeft className="size-4" /> Previous
+                </Button>
+              </Link>
+            )}
+            {nextContent && (
+              <Link
+                href={`/courses/${content.courseId}/modules/content/${nextContent.id}`}
+                className="justify-self-end ml-auto"
+              >
+                <Button variant="outline">
+                  Next <ChevronRight className="size-4" />
+                </Button>
+              </Link>
+            )}
+          </section>
+        </>
       )}
     </main>
   );
--- app/courses/@student/[courseId]/modules/content/[contentId]/page.tsx
+++ app/courses/@student/[courseId]/modules/content/[contentId]/page.tsx
@@ -1,13 +1,20 @@
 import PdfViewer from "@/components/shared/content-page/pdf-viewer";
 import SubmitAssignmentBtn from "@/components/student-access/courses-page/assignment-content/submit-assignment-btn";
 import AssignmentDetails from "@/components/student-access/courses-page/assignment-content/assignment-details";
-import { getContentFile, getModuleContent } from "@/lib/content";
+import {
+  getContentFile,
+  getModuleContent,
+  getModuleContents,
+} from "@/lib/content";
 import { getStudentAssignmentSubmission } from "@/lib/submission";
 import { getOptimisticUser } from "@/lib/user";
 import StudentSubmission from "@/components/student-access/courses-page/student-submission";
 import ContentViewLogger from "@/components/shared/content-page/content-view-logger";
 import ErrorToast from "@/components/ui/error-toast";
 import Searcher from "@/components/shared/search/general-search/searcher";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const ContentPage = async ({
   params,
@@ -40,14 +47,102 @@ const ContentPage = async ({
   const currentDate = content.type === "assignment" ? new Date() : new Date();
 
   const isLocked = currentDate < startDate || currentDate > endDate || false;
+
+  const { success: allContents, error: allContentsError } =
+    await getModuleContents(content.moduleId);
+
+  if (allContentsError || !allContents) {
+    return (
+      <ErrorToast
+        error={"Error fetching module contents: " + allContentsError}
+      />
+    );
+  }
+
+  const currentIdx = allContents?.findIndex((c) => c.id === contentId) ?? -1;
+  const prevContent = currentIdx > 0 ? allContents[currentIdx - 1] : null;
+  const nextContent =
+    allContents && currentIdx < allContents.length - 1
+      ? allContents[currentIdx + 1]
+      : null;
+
   if (isLocked) {
     return (
-      <div className="flex gap-8 p-6">
+      <main className="flex flex-col gap-8 p-6">
+        <section className="flex gap-8">
+          <ContentViewLogger content={content} />
+          <Searcher query={query} page={page} tab={tab} />
+          <div>
+            <div className="flex flex-col gap-3">
+              <h1 className="font-semibold text-3xl">{content.title}</h1>
+              <hr />
+            </div>
+            {content.type === "assignment" && (
+              <AssignmentDetails
+                {...content}
+                submissionsLength={submissions!.length}
+              />
+            )}
+            <p className="mt-4">
+              This content is locked. Please check the start and end dates to
+              see when it will be available.
+            </p>
+          </div>
+          {submissions?.length ? (
+            <StudentSubmission
+              courseId={content.courseId}
+              contentId={content.id}
+            />
+          ) : null}
+        </section>
+        {(prevContent || nextContent) && (
+          <>
+            <hr />
+            <section className="flex justify-between items-center">
+              {prevContent && (
+                <Link
+                  href={`/courses/${content.courseId}/modules/content/${prevContent.id}`}
+                >
+                  <Button variant="outline">
+                    <ChevronLeft className="size-4" /> Previous
+                  </Button>
+                </Link>
+              )}
+              {nextContent && (
+                <Link
+                  href={`/courses/${content.courseId}/modules/content/${nextContent.id}`}
+                  className="justify-self-end ml-auto"
+                >
+                  <Button variant="outline">
+                    Next <ChevronRight className="size-4" />
+                  </Button>
+                </Link>
+              )}
+            </section>
+          </>
+        )}
+      </main>
+    );
+  }
+
+  return (
+    <main className="flex flex-col gap-8 p-6">
+      <section className="flex gap-8">
         <ContentViewLogger content={content} />
         <Searcher query={query} page={page} tab={tab} />
-        <div>
+        <div className="flex-1">
           <div className="flex flex-col gap-3">
-            <h1 className="font-semibold text-3xl">{content.title}</h1>
+            <div className="flex justify-between items-center">
+              <h1 className="font-semibold text-3xl">{content.title}</h1>
+              {content.type === "assignment" && (
+                <SubmitAssignmentBtn
+                  submissionType={content.submissionType!}
+                  contentId={contentId}
+                  submissionsLength={submissions!.length}
+                  maxAttempts={content.maxAttempts}
+                />
+              )}
+            </div>
             <hr />
           </div>
           {content.type === "assignment" && (
@@ -56,10 +151,11 @@ const ContentPage = async ({
               submissionsLength={submissions!.length}
             />
           )}
-          <p className="mt-4">
-            This content is locked. Please check the start and end dates to see
-            when it will be available.
-          </p>
+          <div
+            className="flex flex-col gap-4 mt-6"
+            dangerouslySetInnerHTML={{ __html: content.content }}
+          />
+          {file && <PdfViewer pdfUrl={file.success?.secure_url!} />}
         </div>
         {submissions?.length ? (
           <StudentSubmission
@@ -67,43 +163,33 @@ const ContentPage = async ({
             contentId={content.id}
           />
         ) : null}
-      </div>
-    );
-  }
-  return (
-    <main className="flex gap-8 p-6">
-      <ContentViewLogger content={content} />
-      <Searcher query={query} page={page} tab={tab} />
-      <div className="flex-1">
-        <div className="flex flex-col gap-3">
-          <div className="flex justify-between items-center">
-            <h1 className="font-semibold text-3xl">{content.title}</h1>
-            {content.type === "assignment" && (
-              <SubmitAssignmentBtn
-                submissionType={content.submissionType!}
-                contentId={contentId}
-                submissionsLength={submissions!.length}
-                maxAttempts={content.maxAttempts}
-              />
-            )}
-          </div>
+      </section>
+      {(prevContent || nextContent) && (
+        <>
           <hr />
-        </div>
-        {content.type === "assignment" && (
-          <AssignmentDetails
-            {...content}
-            submissionsLength={submissions!.length}
-          />
-        )}
-        <div
-          className="flex flex-col gap-4 mt-6"
-          dangerouslySetInnerHTML={{ __html: content.content }}
-        />
-        {file && <PdfViewer pdfUrl={file.success?.secure_url!} />}
-      </div>
-      {submissions?.length ? (
-        <StudentSubmission courseId={content.courseId} contentId={content.id} />
-      ) : null}
+          <section className="flex justify-between items-center">
+            {prevContent && (
+              <Link
+                href={`/courses/${content.courseId}/modules/content/${prevContent.id}`}
+              >
+                <Button variant="outline">
+                  <ChevronLeft className="size-4" /> Previous
+                </Button>
+              </Link>
+            )}
+            {nextContent && (
+              <Link
+                href={`/courses/${content.courseId}/modules/content/${nextContent.id}`}
+                className="justify-self-end ml-auto"
+              >
+                <Button variant="outline">
+                  Next <ChevronRight className="size-4" />
+                </Button>
+              </Link>
+            )}
+          </section>
+        </>
+      )}
     </main>
   );
 };
