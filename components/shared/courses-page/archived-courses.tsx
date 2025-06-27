93a5ed251d3b6bfe25bd2f6740dc59ec98f44446
Richard William <rwilliam.flores@gmail.com>
2025-06-27 12:36:53 +0800

    Fixed type error

--- components/shared/courses-page/archived-courses.tsx
+++ components/shared/courses-page/archived-courses.tsx
@@ -1,5 +1,9 @@
 import { getArchivedCourses, getArchivedInstructorCourses } from "@/lib/course";
-import { getOptimisticUser } from "@/lib/user";
+import {
+  getOptimisticUser,
+  getUnarchivedInstructors,
+  getUnarchivedStudents,
+} from "@/lib/user";
 import CourseCard from "./course-card";
 import ErrorToast from "@/components/ui/error-toast";
 
@@ -9,9 +13,23 @@ const ArchivedCourses = async () => {
     user.role === "instructor"
       ? await getArchivedInstructorCourses(user.id)
       : await getArchivedCourses();
+  const { success: instructors, error: instructorsError } =
+    await getUnarchivedInstructors();
+  const { success: students, error: studentsError } =
+    await getUnarchivedStudents();
 
   if (error || !courses) {
     return <ErrorToast error={"Error fetching archived courses: " + error} />;
+  }
+
+  if (studentsError || !students) {
+    return <ErrorToast error={"Error fetching students: " + studentsError} />;
+  }
+
+  if (instructorsError || !instructors) {
+    return (
+      <ErrorToast error={"Error fetching instructors: " + instructorsError} />
+    );
   }
 
   return (
@@ -19,7 +37,14 @@ const ArchivedCourses = async () => {
       {!courses.length ? (
         <p className="text-muted-foreground">No courses found</p>
       ) : (
-        courses.map((course) => <CourseCard key={course.id} {...course} />)
+        courses.map((course) => (
+          <CourseCard
+            instructors={instructors}
+            students={students}
+            key={course.id}
+            {...course}
+          />
+        ))
       )}
     </section>
   );
